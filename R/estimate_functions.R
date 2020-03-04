7f53b3d50ea8da9b44012989d37cd6035044d8e7
ASteiner <aleksandra.steiner@gmail.com>
2020-03-04 12:04:41 +0100

    adds development code for solving the system of equations

--- R/estimate_functions.R
+++ R/estimate_functions.R
@@ -1,14 +1,3 @@
-calculate_c_os <- function(n_observations, n_variables, gamma, 
-                           alfa_star = 1.1678, sigma_star = 3.3466) {
-  numerator <- n_observations*gamma*alfa_star
-  denominator <-((sigma_star^2)*n_variables + n_observations*gamma*(alfa_star^2))
-  numerator/denominator
-}
-
-as_estimator <- function(mle_estimator, c_as) {
-  mle_estimator * c_as
-}
-
 #' @importFrom stats runif
 get_kappa_grid <- function(method, n_kappa, kappa) {
   if(method == 'random') {
@@ -26,10 +15,11 @@ is_feasible <- function(X, Y) {
                         ifelse(Y == 0, -1, 1)) 
   #we do not put zero because of weak inequality
   solution_object[28] == 0 
-  #if true, the problem is feasible, so MLE does not exist
+  #if true, the problem is feasible = MLE does not exist
 }
 
-#caclulates how often MLE doesn't exist for i-th kappa
+#calculates how often MLE doesn't exist for i-th kappa, 
+#so it calculates pi for one kappa
 estimate_pi_i <- function(X, Y, n_rep, kappa_i) { 
   n_variables <- ncol(X)
   n_observations <- nrow(X)
@@ -49,6 +39,7 @@ estimate_pi <- function(X, Y, n_rep, kappa_grid) {
   })
 }
 
+#finds a pair of boundary kappas to find then such kappa for which pi = 0.5 
 find_boundary <- function(kappa_grid, pi_grid) {
   j <- which(pi_grid >= 0.5)[1]
   if(j > 1) {
@@ -57,9 +48,17 @@ find_boundary <- function(kappa_grid, pi_grid) {
          pi_hat_kappa_i = pi_grid[j - 1], 
          pi_hat_kappa_j = pi_grid[j])
   } else if (j == 1) {
-    #use new kappa < kappa_1 and corresponding pi ?
+    else if(pi_grid[1] == 0.5) {
+      list(kappa_i = kappa_grid[j], 
+           kappa_j = kappa_grid[j], 
+           pi_hat_kappa_i = pi_grid[j], 
+           pi_hat_kappa_j = pi_grid[j] + 1)
+    }
+    else if(pi_grid[1] > 0.5) {
+      #Is it possible and should be kappa <- kappa_grid[1] found ?
+    }
   } else if (j == integer(0)) {
-    stop('no kappa estimate exists, try with finer grid')  }
+    stop('no kappa estimate exists, try with a finer grid')  } #is it a correct message ?
 }
 
 #' @importFrom stats approx
@@ -81,14 +80,61 @@ estimate_kappa <- function(X, Y, n_rep = 10,
   #interpolation solution for kappa_hat (x)
 }
 
+#' @importFrom R.matlab readMat
 estimate_gamma <- function(kappa_hat) {
-  #TODO: set gamma_hat = g_MLE(kappa_hat)
+  kappa <- kappa_gamma_data[,1] == round(kappa_hat, digits = 3) #round/floor ?
+  kappa_gamma_data[kappa, 2]
 }
 
+#two-dimensional density
+calculate_Q_density <- function(q1, q2, alpha, gamma, kappa, sigma) {
+  exp((-q1^2 * ( (alpha * gamma)^2 + kappa * sigma^2)  
+       - 2 * q1 * q2 * alpha * gamma^2
+       - q2^2 * gamma^2) / 2 * kappa * sigma^2) 
+  / (2 * pi * sigma * sqrt(kappa))
+}
+
+prox_function <- function(z) {
+  
+}
+
+#functions from system of equations
+h_function <- function(q1, q2) {
+  (2 * lambda^2 * exp(q1) * exp(2 * prox(q2))) /
+    ((1 + exp(q1)) * (1 + exp(prox(q2)))^2)
+}
+
+g_function <- function(q1, q2) {
+  (lambda * q1 * exp(q1) * exp(prox(q2))) /
+    ((1 + exp(q1)) * (1 + exp(prox(q2))))
+}
+
+j_function <- function(q1, q2) {
+  (2 * exp(q1) * (1 + exp(prox(q2)))^2) /
+    ((1 + exp(q1)) * ((1 + exp(prox(q2)))^2 + lambda * exp(prox(q2))))
+}
+
+calculate_values <- function(alpha, sigma, lambda) {
+  y <- numeric(3)
+  y[1] <- (1/kappa^2) * adaptIntegrate(h_function, 
+                               lowerLimit = c(-infinity, -infinity), 
+                               upperLimit = c(+infinity, +infinity)) - sigma^2
+  y[2] <- adaptIntegrate(g_function, 
+                         lowerLimit = c(-infinity, -infinity), 
+                         upperLimit = c(+infinity, +infinity))
+  y[3] <- adaptIntegrate(h_function, 
+                         lowerLimit = c(-infinity, -infinity), 
+                         upperLimit = c(+infinity, +infinity)) + kappa - 1
+  y
+}
+
+#' @importFrom nleqslv nleqslv
 solve_equations <- function(kappa, gamma) {
-  alpha <- 1
-  sigma <- 1
-  lambda <- 1
+  xstart <- c() #what should be the starting point here ?
+  solution <- nleqslv(xstart, calculate_values)$x #control options
+  alpha <- solution[1]
+  sigma <- solution[2]
+  lambda <- solution[3]
   list(alpha = alpha, sigma = sigma, lambda = lambda)
 }
 
@@ -106,4 +152,19 @@ get_gamma_estimator <- function(X, Y, estimate_gamma) {
 get_mle_estimator <- function(Y, X, n_variables) {
   logistic_model <- glm(Y ~ X, family = binomial)
   coef(logistic_model)[2 : (n_variables + 1)]
+}
+
+calculate_c_os <- function(n_observations, n_variables, gamma, 
+                           alfa_star = 1.1678, sigma_star = 3.3466) {
+  numerator <- n_observations*gamma*alfa_star
+  denominator <-((sigma_star^2)*n_variables + n_observations*gamma*(alfa_star^2))
+  numerator/denominator
+}
+
+get_as_estimator <- function(mle_estimator, c_as) {
+  mle_estimator * c_as
+}
+
+get_ec_estimator <- function(mle_estimator, alpha_star) {
+  mle_estimator / alpha_star
 }
