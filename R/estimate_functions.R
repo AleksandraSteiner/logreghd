97cbd4287fcb5fdd1db54b93c5aacc62cd5ff804
ASteiner <aleksandra.steiner@gmail.com>
2020-04-22 14:35:32 +0200

    corrects density and integral functions in estimate_functions

--- R/estimate_functions.R
+++ R/estimate_functions.R
@@ -8,18 +8,13 @@ get_kappa_grid <- function(method, n_kappa, kappa) {
 }
 
 #' @importFrom lpSolve lp
-#checks if MLE exists
 is_feasible <- function(X, Y) { 
   solution_object <- lp("min", 0, X, 
                         ifelse(Y == 0, "<=", ">="), 
-                        ifelse(Y == 0, -1, 1)) 
-  #we do not put zero because of weak inequality
-  solution_object[28] == 0 
-  #if true, the problem is feasible = MLE does not exist
+                        ifelse(Y == 0, -1, 1))
+  solution_object[28] == 0
 }
 
-#calculates how often MLE doesn't exist for i-th kappa, 
-#so it calculates pi for one kappa
 estimate_pi_i <- function(X, Y, n_rep, kappa_i) { 
   n_variables <- ncol(X)
   n_observations <- nrow(X)
@@ -30,7 +25,6 @@ estimate_pi_i <- function(X, Y, n_rep, kappa_i) {
   sum(feasibile)/n_rep
 }
 
-#caluculates pi_hats for kappa grid
 estimate_pi <- function(X, Y, n_rep, kappa_grid) {
   n_variables <- ncol(X)
   n_observations <- nrow(X)
@@ -39,7 +33,6 @@ estimate_pi <- function(X, Y, n_rep, kappa_grid) {
   })
 }
 
-#finds a pair of boundary kappas to find then such kappa for which pi = 0.5 
 find_boundary <- function(kappa_grid, pi_grid) {
   j <- which(pi_grid >= 0.5)[1]
   if(j > 1) {
@@ -55,7 +48,7 @@ find_boundary <- function(kappa_grid, pi_grid) {
            pi_hat_kappa_i = pi_grid[j], 
            pi_hat_kappa_j = pi_grid[j] + 1)
     }
-    else if (pi_grid[1] > 0.5) { #down interpolation
+    else if (pi_grid[1] > 0.5) {
       list(kappa_i = kappa_grid[j], 
            kappa_j = kappa_grid[j + 1], 
            pi_hat_kappa_i = pi_grid[j], 
@@ -80,20 +73,21 @@ estimate_kappa <- function(X, Y, n_rep = 10,
   kappa_j <- boundary[2]
   pi_j_1 <- boundary[3]
   pi_j <- boundary[4]
-  kappa_j_1 + (0.5 - pi_j_1) * (kappa_j - kappa_j_1) / (pi_j - pi_j_1) 
-  #interpolation solution for kappa_hat (x)
+  kappa_j_1 + (0.5 - pi_j_1) * (kappa_j - kappa_j_1) / (pi_j - pi_j_1)
 }
 
 estimate_gamma <- function(kappa_hat) {
-  kappa <- kappa_gamma_data[, 1] == round(kappa_hat, digits = 3) #round/floor ?
+  kappa <- kappa_gamma_data[, 1] == round(kappa_hat, digits = 3) #CHECK: round/floor ?
   kappa_gamma_data[kappa, 2]
 }
 
-calculate_Q_density <- function(alpha, sigma, kappa, gamma) {
-  function(q1, q2) {
-    exp((-q1 ^ 2 * ( (alpha * gamma) ^ 2 + kappa * sigma ^ 2)  
-         - 2 * q1 * q2 * alpha * gamma ^ 2 - q2 ^ 2 * gamma ^ 2) / 2 * kappa * sigma ^ 2) /
-      (2 * pi * sigma * sqrt(kappa))
+calculate_Q_density <- function(alpha, sigma, lambda, kappa, gamma = sqrt(5)) {
+  sigma_1 = gamma
+  sigma_2 = sqrt(alpha ^ 2 * gamma ^ 2 + kappa * sigma ^ 2)
+  rho = -alpha * gamma ^ 2
+  function(q) {
+    z = q[1] ^ 2 / sigma_1 ^ 2 - (2 * rho * q[1] * q[2]) / (sigma_1 * sigma_2) + q[2] ^ 2 / sigma_2 ^ 2
+    (1 / (2 * pi * sigma_1 * sigma_2 * sqrt(1 - rho ^ 2))) * exp(- z / (2 * (1 - rho ^ 2)))
   }
 }
 
@@ -111,32 +105,35 @@ prox <- function(z, lambda) {
 
 integral_1 <- function(alpha, sigma, lambda, kappa, gamma) {
   function(q) {
-    calculate_Q_density(alpha, gamma, kappa, sigma) *
-      ((2 * lambda ^ 2 * exp(q[1]) * exp(2 * prox(q[2], lambda))) /
-         ((1 + exp(q[1])) * (1 + exp(prox(q[2], lambda))) ^ 2 * kappa ^ 2)) - sigma ^ 2
+    (calculate_Q_density(alpha, gamma, kappa, sigma))(q) *
+      ((2 * exp(q[1]) / (1 + exp(q[1]))) *
+         (lambda * (exp(prox(q[2], lambda)) / (1 + exp(prox(q[2], lambda))))) ^ 2) /
+      kappa ^ 2 -
+      sigma ^ 2
   }
 }
 
 integral_2 <- function(alpha, sigma, lambda, kappa, gamma) {
   function(q) {
-    calculate_Q_density(alpha, gamma, kappa, sigma) *
-      (lambda * q[1] * exp(q[1]) * exp(prox(q[2], lambda))) /
+    (calculate_Q_density(alpha, gamma, kappa, sigma))(q) *
+      (exp(q[1]) * exp(prox(q[2], lambda)) * lambda * q[1]) /
       ((1 + exp(q[1])) * (1 + exp(prox(q[2], lambda))))
   }
 }
 
 integral_3 <- function(alpha, sigma, lambda, kappa, gamma) {
   function(q) {
-    calculate_Q_density(alpha, gamma, kappa, sigma) *
-      (2 * exp(q[1]) * (1 + exp(prox(q[2], lambda))) ^ 2) /
-      ((1 + exp(q[1])) * ((1 + exp(prox(q[2], lambda))) ^ 2 
-                          + lambda * exp(prox(q[2], lambda)))) + kappa - 1
+    (calculate_Q_density(alpha, gamma, kappa, sigma))(q) *
+      ((2 * exp(q[1])) / (1 + exp(q[1]))) /
+      (1 + (lambda * exp(prox(q[2], lambda))) / (1 + exp(prox(q[2], lambda))) ^ 2) +
+      kappa - 1
   }
 }
 
 #' @importFrom cubature adaptIntegrate
 calculate_integral <- function(alpha, sigma, lambda, kappa, gamma, integral) {
-  adaptIntegrate(integral, c(-Inf, -Inf), c(+Inf, +Inf))[[1]]
+  integr_func = integral(alpha, sigma, lambda, kappa, gamma)
+  adaptIntegrate(integr_func, c(-Inf, -Inf), c(+Inf, +Inf))[[1]]
 }
 
 system_values <- function(kappa, gamma) {
@@ -149,7 +146,7 @@ system_values <- function(kappa, gamma) {
 
 #' @importFrom nleqslv nleqslv
 solve_equations <- function(kappa, gamma) {
-  xstart <- c(1.1678, 3.3466, 0.9605) #these are solutions for kappa = 0.1 and gamma = sqrt(5)
+  xstart <- c(1.1678, 3.3466, 0.9605) #solutions for kappa = 0.1 & gamma = sqrt(5)
   system_values <- system_values(kappa, gamma)
   solution <- nleqslv(xstart, system_values)$x 
   names(solution) <- c("alpha", "sigma", "lambda")
